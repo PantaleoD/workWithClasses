8de87149518d0239b2de9e20cd7b5c86e8e3ffa7
David Barnes <barnesdavidj@gmail.com>
2015-10-12 16:44:34 -0400

    Add small demos of everthing that should help with understanding.

---
+++ app/src/main/java/edu/kvcc/cis298/workwithclasses/workwithclasses/Calc.java
@@ -0,0 +1,11 @@
+package edu.kvcc.cis298.workwithclasses.workwithclasses;
+
+/**
+ * Created by dbarnes on 10/12/2015.
+ */
+public class Calc {
+
+    public double multiplyByFour(double numberToMultiply) {
+        return (numberToMultiply*4);
+    }
+}
